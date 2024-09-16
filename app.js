ebe5558f9635fdaca0a6002486d569b5c27fb04a
rajnish ranjan <ranjanrajnish64@gmail.com>
2024-09-16 23:08:42 +0530

    Inception

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {id: "headingggg"}, "Hello World From React JS");
+
+console.log(heading)
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
