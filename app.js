3af81aa163662efba3e357cf007bd5afb28fdd02
rajnish ranjan <ranjanrajnish64@gmail.com>
2024-09-18 23:10:17 +0530

    JSX entry intro is done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const heading = React.createElement("h1", {id: "headingggg"}, "Hello World From React JS inside app");
+const heading = React.createElement("h1", {id: "headingggg"}, "Hello World From React inside the JS ");
 
 console.log(heading)
 
---
+++ foundation_app.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+//react elements
+
+// react element is an object
+// when we render this element on dom than this becomes a html element
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 🚀"
+);
+// jsx is separate html like syntax 
+
+const jsxHeading = <h1 id ="heading"> Namaste React using JSX ⭐️</h1>
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
