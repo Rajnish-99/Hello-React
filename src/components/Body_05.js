d0a96eff60815ffe25b99dcef444076de1b65c2f
rajnish ranjan <ranjanrajnish64@gmail.com>
2024-09-29 23:52:10 +0530

    Search funcitonality on its way

--- src/components/Body_05.js
+++ src/components/Body_05.js
@@ -1,10 +1,11 @@
 import RestaurnantCards from "./RestaurantCards";
-import resList from "../utils/mockData";
+// import resList from "../utils/mockData";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
-
+  const [listOfRestaurants, setlistOfRestaurants] = useState([]);
+  const [searchText,setsearchText] = useState("")
   // first body will render than the useEffect will be callled
   useEffect(() => {
     fetchData();
@@ -19,33 +20,53 @@ const Body = () => {
     console.log(json);
 
     console.log(
-      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
     );
+
+    // this is optional chaining which will prevent the code from error and breakage
     setlistOfRestaurants(
-      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+
     console.log(listOfRestaurants);
   };
 
-  // Normal Js variable
+  // conditional rendering
+  // we use shimmer UI
 
-  let listOfRestaurants2 = [];
-
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      <div className="filter"></div>
-      <button
-        className="filter-btn"
-        onClick={() => {
-          const filterList = listOfRestaurants.filter(
-            (res) => res.info.avgRating > 4.4
-          );
-          setlistOfRestaurants(filterList);
-        }}
-      >
-        Top Rated Restaurant
-      </button>
+      <div className="filter">
+        <div className="search">
+          <input type="text" className="searh-box"></input>
+          <button
+            className="search-button" value={searchText} onChange={(e)=>{setsearchText(e.target.value)}}
+            onClick={() => {
+              // filter the restaurnant cards and update the UI accordingly
+              alert("Ranu beta masti nahi");
+              console.log(searchText)
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setlistOfRestaurants(filterList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+
       <div className="res-container">
+        {console.log(listOfRestaurants)}
         {listOfRestaurants.map((info, index) => (
           <RestaurnantCards key={index} resData={info} />
         ))}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,40 @@
-import { LOGO_URL } from "../utils/constants"
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 // functional component of the header
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={LOGO_URL}
-          />
-        </div>
-  
-        <div className="nav-item">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  // let btnName = "Login";
+  const [btnNameReact, setbtnNameReact] = useState("Login");
+  console.log("Header Rendered called again and again")
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
       </div>
-    );
-  };
-// before import we have to first export first give than take 
-  export default Header;
+
+      <div className="nav-item">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="Login-button"
+            onClick={() => {
+              // btnName = "Logout";
+              // toggle between login and logout 
+              btnNameReact ==="Login"? setbtnNameReact("Logout"): setbtnNameReact("Login");
+              // setbtnNameReact("Logout");
+              // alert("Button Dab gya lakin kcuh hua nahi h")
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+``;
+// before import we have to first export first give than take
+export default Header;
