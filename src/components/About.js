ad5c36b33c9db86382e0fde1e101071ae6259833
rajnish ranjan <ranjanrajnish64@gmail.com>
2024-10-05 15:37:29 +0530

    Class state management

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,41 @@
 import User from "./User";
 import UserClass from "./UserClass"; 
+import React from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <h2>This is React deep dive</h2>
-      <User name ={"Rajnish Ranjan(Function)"}/>
-      <UserClass name={"Rajnish Ranjan (Class)"} location={"Dehradun (Class)"}/>
-    </div>
-  );
-};
+// creating a class based component of the below component
+
+class About extends React.Component{
+  constructor(props){
+    super(props);
+
+    console.log("parent constructor called");
+  }
+  componentDidMount(){ console.log("Parent child is mounted ")  }
+  render(){
+    console.log("parent render called");
+    return(
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is React deep dive</h2>
+        {/* <User name ={"Rajnish Ranjan(Function)"}/> */}
+        <UserClass name={"Rajnish Ranjan (Class)"} location={"Dehradun (Class)"}/>
+      </div>
+    );
+  }
+}
+
+
+
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About Us</h1>
+//       <h2>This is React deep dive</h2>
+//       {/* <User name ={"Rajnish Ranjan(Function)"}/> */}
+//       <UserClass name={"Rajnish Ranjan (Class)"} location={"Dehradun (Class)"}/>
+//     </div>
+//   );
+// };
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,10 +4,28 @@
 
     constructor(props){
         super(props);
-        console.log(props)
+        // earlier we used useState hook to maintain state
+        // now we will use this.state
+        // this.state is an object
+        this.state = {
+            count :0,
+            count2:2,
+        };
+        console.log("child constructor called");
+    }
+
+    componentDidCatch(){
+        console.log("Parent componentDidCatch did mount called");
     }
     render(){
+        console.log("child render called");
         return(<div className="user-card">
+            <h1>Count= {this.state.count}</h1>
+            {/* <h1>Count2= {this.state.count2}</h1> */}
+            <button onClick={()=>{
+                this.setState({count:this.state.count+1});
+            }}>Increment</button> 
+            {console.log(this.statecount)}
             <h2>Name:{this.props.name}</h2>
             <h3>Location:{this.props.location}</h3>
         </div>);
