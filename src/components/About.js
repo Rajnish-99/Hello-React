ad5e91299e05cbb6c95102e26fe69201fb98b4d9
rajnish ranjan <ranjanrajnish64@gmail.com>
2024-10-13 22:13:40 +0530

    Changes

--- src/components/About.js
+++ src/components/About.js
@@ -18,8 +18,8 @@ class About extends React.Component{
         <h1>About Class Component</h1>
         <h2>This is React deep dive</h2>
         {/* <User name ={"Rajnish Ranjan(Function)"}/> */}
-        <UserClass name={"Rajnish Ranjan (Class1)"} location={"Dehradun (Class1)"}/>
-        <UserClass name={"Rajnish Ranjan (Class2)"} location={"Dehradun (Class2)"}/>
+        <UserClass name={"Rajnish Ranjan (Class 1)"} location={"Dehradun (Class 1)"}/>
+        <UserClass name={"Rajnish Ranjan (Class 2)"} location={"Dehradun (Class 2)"}/>
       </div>
     );
   }
