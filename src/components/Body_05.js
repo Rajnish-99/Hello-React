73a0179b1e584ee4d1c63ba4cb1893c2565d41d5
Rajnish-99 <ranjanrajnish64@gmail.com>
2024-11-21 17:55:43 +0530

    pata nahi changes kar die maine

--- src/components/Body_05.js
+++ src/components/Body_05.js
@@ -42,11 +42,12 @@ const Body = () => {
   };
 
   const onlineStatus = useOnlineStatus();
-  if(onlineStatus=== false)
-    return(
-      <h1>Looks like you are offline !!  please check your internet connection</h1>
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you are offline !! please check your internet connection
+      </h1>
     );
-  
 
   // conditional rendering
   // we use shimmer UI
@@ -87,17 +88,17 @@ const Body = () => {
           </button>
         </div>
         <div className="search m-4 p-4 flex items-center">
-        <button
-          className="px-4 py-2 bg-gray-100 rounded-lg"
-          onClick={() => {
-            const filterList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4.1
-            );
-            setFilteredRestaurants(filterList);
-          }}
-        >
-          Top Rated Restaurant
-        </button>
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              const filterList = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4.1
+              );
+              setFilteredRestaurants(filterList);
+            }}
+          >
+            Top Rated Restaurant
+          </button>
         </div>
       </div>
 
@@ -117,5 +118,3 @@ const Body = () => {
 };
 
 export default Body;
-
-
